0c4a57ca73de0a5ab695814d6c462ca02dd8c7c7
Kylejeong2 <Kylejeong21@gmail.com>
2024-01-10 21:36:15 -0800

    first commit

---
+++ app.py
@@ -0,0 +1,21 @@
+from dotenv import find_dotenv, load_dotenv
+from transformers import pipeline
+
+load_dotenv(find_dotenv())
+
+# img2text
+def img2text(url):
+    image_to_text = pipeline("image-to-text", model="Salesforce/blip-image-captioning-base")
+
+    text = image_to_text(
+        url
+    )[0]['generated text']
+
+    print(text)
+    return text
+
+img2text("license.jpg")
+
+#llm
+
+#text to speech
